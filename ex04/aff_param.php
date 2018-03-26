e8fb5160913ea6ab1c2dac9ad973ded6f7a81c10
pleroux <p.leroux@gmx.com>
2018-03-26 14:04:09 +0200

    ex04

---
+++ ex04/aff_param.php
@@ -0,0 +1,15 @@
+#!/usr/bin/php
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pierre
+ * Date: 26/03/18
+ * Time: 13:53
+ */
+
+foreach ($argv as $key => $val) {
+    if ($key != 0)
+        echo "$val\n";
+}
+
+?>
