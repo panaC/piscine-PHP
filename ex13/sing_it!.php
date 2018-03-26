166980bf573d126867b63913091f697ff9d9b09b
pleroux <p.leroux@gmx.com>
2018-03-26 19:47:33 +0200

    ex13

--- ex13/sing_it!.php
+++ ex13/sing_it!.php
@@ -1,3 +1,4 @@
+#!/usr/bin/php
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +6,22 @@
  * Date: 26/03/18
  * Time: 19:41
  */
+if ($argc != 2)
+    return;
+
+if ($argv[1] == "mais pourquoi cette demo ?")
+{
+    echo "Tout simplement pour qu'en feuilletant le sujet\n";
+    echo "on ne s'apercoive pas de la nature de l'exo\n";
+}
+if ($argv[1] == "mais pourquoi cette chanson ?")
+    echo "Parce que Kwame a des enfants\n";
+if ($argv[1] == "vraiment ?")
+{
+    if (rand (0, 1))
+        echo "Nan c'est parce que c'est le premier avril\n";
+    else
+        echo "Oui il a vraiment des enfants\n";
+}
+
+?>
