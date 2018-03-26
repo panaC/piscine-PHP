7f10b5962201e4d46f37202b28f35a436a0fe934
pleroux <p.leroux@gmx.com>
2018-03-26 14:58:04 +0200

    ex05

---
+++ ex05/epur_str.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pierre
+ * Date: 26/03/18
+ * Time: 14:04
+ */
+
+if ($argc == 2){
+    echo preg_replace("/\s+/", ' ', trim($argv[1]));
+}
+
+?>
